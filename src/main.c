5a1bb744de6c70b99d355a7b51590f7444967a8a
Jean-Michel MARINO <public.jmm@gmail.com>
2019-06-22 18:44:53 +0200

    Cleaning code and distrib optimization

--- src/main.c
+++ src/main.c
@@ -83,7 +83,7 @@
 
 /* -- global variables -- */
 
-const char   *gAppVersion = "zntpdate v0.4.15"; /*!< Application version.   */
+const char   *gAppVersion = "zntpdate v0.4.16"; /*!< Application version.   */
 
 options_t    gAppOptions;                       /*!< Application options.   */
 trace_desc_t *gAppTrace;                        /*!< App log structure      */
